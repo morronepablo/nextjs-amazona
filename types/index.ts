e75c1d6788370a4c4bfb6225a6f3a1f728ea2735
morronepablo <morronepablo@gmail.com>
2025-02-12 14:41:34 -0300

    18-place-order

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import {
   CartSchema,
+  OrderInputSchema,
   OrderItemSchema,
   ProductInputSchema,
   ShippingAddressSchema,
@@ -26,6 +27,7 @@ export type Data = {
   }[];
 };
 
+export type IOrderInput = z.infer<typeof OrderInputSchema>;
 export type OrderItem = z.infer<typeof OrderItemSchema>;
 export type Cart = z.infer<typeof CartSchema>;
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
