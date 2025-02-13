2f86ab958ae81f3021fcbf4086c0611a580a5e03
morronepablo <morronepablo@gmail.com>
2025-02-13 17:24:50 -0300

    27-create-admin-dashboard

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -11,13 +11,13 @@ import CartButton from "./cart-button";
 import UserButton from "./user-button";
 import ThemeSwitcher from "./theme-switcher";
 
-export default function Menu() {
+export default function Menu({ forAdmin = false }: { forAdmin?: boolean }) {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
         <ThemeSwitcher />
         <UserButton />
-        <CartButton />
+        {forAdmin ? null : <CartButton />}
       </nav>
       <nav className="md:hidden">
         <Sheet>
